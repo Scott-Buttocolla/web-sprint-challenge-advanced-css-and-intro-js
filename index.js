6222dcc604bca42128d8fd848e5eb7d0160e851c
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-10-09 09:36:32 -0700

    Finished Task 3

--- index.js
+++ index.js
@@ -230,11 +230,10 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
-
-  
+function getArtistByIndex(artists, index){
+  return `the artist at index ${artists[index].id} is ${artists[index].name}`;
+  }
+  console.log(getArtistByIndex(artists, 0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
